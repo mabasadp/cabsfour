ebf28c9178364da6d91a08abf144a3146456dd05
mabasadp <110773312+mabasadp@users.noreply.github.com>
2023-09-09 08:23:06 +0800

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  env: {
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    MONGODB_URI: process.env.MONGODB_URI,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+  },
+  images: {
+    domains: ["res.cloudinary.com"],
+  },
+};
+
+module.exports = nextConfig;
